09812c8669722fe72b9bd16f8952c745d0397ce2
Elbert Jean <elbertjeandossantos@gmail.com>
2021-09-17 18:58:38 -0300

    Estudos 17.09.21

--- Alura - Avançando com PHP/banco.php
+++ Alura - Avançando com PHP/banco.php
@@ -19,14 +19,22 @@ $contasCorrentes = [
     ]
 ];
 
+unset ($contasCorrentes["123.456.722-10"]);
+
 titularComLetrasMaiusculas($contasCorrentes["123.456.234-10"]);
 
 $contasCorrentes["123.456.789-10"] = depositar($contasCorrentes["123.456.789-10"], 1000);
 $contasCorrentes["123.456.234-10" ] = sacar($contasCorrentes["123.456.234-10" ], 200);
 
 foreach ($contasCorrentes as $indice => $valor) {
-    exibeMensagem("CPF: $indice : {$valor["titular"]} | Saldo : {$valor["saldo"]}");
-   
+    //list ("titular" => $titular , "saldo" => $saldo) = $valor;
+    ["titular" => $titular , "saldo" => $saldo]  = $valor;
+
+    exibeMensagem("CPF: $indice");
+    exibeMensagem("Titular: $titular");
+    exibeMensagem("Saldo $saldo");
+    exibeMensagem("");
+    //exibeMensagem("CPF: $indice : {$valor["titular"]} | Saldo : {$valor["saldo"]}");
     //Também pode se utilizar sem as chaves, basta utilizar o parâmetro sem aspas dentro do conchetes
     //exibeMensagem("CPF: $indice : $valor[titular] | Saldo : $valor[saldo]");
 }
